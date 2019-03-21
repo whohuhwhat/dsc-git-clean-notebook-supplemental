f35a166cbdf765c7de71c08c0ec5f6e7e3041fa0
Florencia Leoni <florenleoni@gmail.com>
2019-03-21 13:40:09 -0400

    example

---
+++ importme.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+df = pd.read_csv('baseball_stats.csv')
+teams = df.name.unique()
+
+def example_function(name):
+    df["team_ratio"] = df["wins"]/df["runs_scored"]
+    team_ratio = df[df.name == name]["year", "team_ratio"]
+    plt.bar(team_ratio["year"], team_ratio["team_ratio"])
